a23e16a82501f6059f13dfa64a00c4659adc1e79
Hol1k <danilugryumov96@gmail.com>
2024-04-20 12:31:28 +0500

    first commit

---
+++ BasicDataTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+Console.WriteLine("***** Fun with Basic Data Types *****\n");
+
+static void LocalVarDeclaraions()
+{
+    Console.WriteLine("=> Data Declarations:");
+    int myInt = 0;
+    string myString = "This is my character data";
+    bool b1 = true, b2 = false, b3 = b1;
+    Console.WriteLine();
+}
---
+++ Chapter3_AllProject/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Title = "Ahah";
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+
+Console.ReadLine();
